a941d9e0feb173ac60f2429dbdb55384cef3d3f9
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-20 22:21:52 +0200

    studied about polymorphism

---
+++ src/CheckBox.java
@@ -0,0 +1,9 @@
+public class CheckBox extends UIControl{
+    @Override
+    public void render(){
+        System.out.println("Render checkbox");
+    }
+}
+//UIControl[] controls = {new TextBox(), new CheckBox()};
+//        for(var control: controls)
+//        control.render();
