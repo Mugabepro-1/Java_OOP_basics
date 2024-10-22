4a55dd3d21568d0984cea3bd152db2b4c59e03be
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-22 22:04:22 +0200

    Applied interfaced to calculate tax in different ways

---
+++ src/TaxReport.java
@@ -0,0 +1,15 @@
+public class TaxReport {
+    public TaxCalculator calculator;
+
+    public TaxReport(TaxCalculator calculator){
+        this.calculator = calculator;
+    }
+    public void show(){
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
+
+    public void setCalculator(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
+}
